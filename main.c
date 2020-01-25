9a810441c929ea333bb168e5c7c677fede46d67c
Nathan Bessa <nathan.besssa@gmail.com>
2020-01-25 22:32:08 +0100

    feat(thread): add first threaded version of program

--- main.c
+++ main.c
@@ -20,7 +20,14 @@ typedef struct ImageToTreat {
     Image out;
     int treated;
     int writed;
+    int applying_treatment;
 }ImageToTreat;
+
+typedef struct ArgForThread {
+    ImageToTreat* images;
+    int image_count;
+    enum EffectType effect;
+}ArgForThread;
 
 char* create_path(char* path, char* imageName) {
     char* full_path = malloc(strlen(path) + strlen(imageName) + 1);
@@ -43,6 +50,7 @@ void list_image(char* dir_path, ImageToTreat** listImage, int imageCount, char* 
             list[count].img = open_bitmap(list[count].path);
             list[count].treated = 0;
             list[count].writed = 0;
+            list[count].applying_treatment = 0;
             list[count].path_out = create_path(dir_out, entry->d_name);
             count++;
         }
@@ -67,8 +75,9 @@ int count_img_in_dir(char* dir_path){
 
 int find_image_to_treat(int imageCount, ImageToTreat* listImage){
     for(int i = 0; i < imageCount; i++){
-        printf("\n%d//%d--%d\n", listImage[i].treated, listImage[i].writed, i);
-        if(listImage[i].treated != 1){
+        printf("\n%d//%d--%d", listImage[i].treated, listImage[i].writed, i);
+        if(listImage[i].treated != 1 && listImage[i].applying_treatment != 1){
+            listImage[i].applying_treatment = 1;
             return i;
         }
     }
@@ -77,7 +86,7 @@ int find_image_to_treat(int imageCount, ImageToTreat* listImage){
 
 int find_image_to_write(int imageCount, ImageToTreat* listImage){
     for(int i = 0; i < imageCount; i++){
-        printf("\n%d//%d--%d\n", listImage[i].treated, listImage[i].writed, i);
+        printf("\n%d//%d--%d", listImage[i].treated, listImage[i].writed, i);
         if(listImage[i].writed != 1 && listImage[i].treated == 1){
             return i;
         }
@@ -85,27 +94,14 @@ int find_image_to_write(int imageCount, ImageToTreat* listImage){
     return -1;
 }
 
-void write_image_to_bitmap(ImageToTreat* listimg, int count) {
-    //int index = find_image_to_write(count, listimg);
-    int index = find_image_to_write(count, listimg);
-    printf("\n%d/-/%s\n", listimg[index].out.bmp_header.width, listimg[index].path_out);
-    if(index == -1) {
-        return;
-    }
-    save_bitmap(listimg[index].out, listimg[index].path_out);
-    listimg[index].writed = 1;
+void write_image_to_bitmap(ImageToTreat* listimg) {
+    save_bitmap(listimg->out, listimg->path_out);
+    listimg->writed = 1;
 }
 
-void treat_image(ImageToTreat* img, int count, enum EffectType effect){
-    //printf("%s/-/%s/-/%d\n", img->path, img->path_out, img->img.bmp_header.width);
-    //int index = find_image_to_treat(count, &img);
-    //printf("index : %d\n", index);
-    int index = find_image_to_treat(count, img);
-    if(index == -1) {
-        return;
-    }
-    apply_effect(&img[index].img, &img[index].out, effect);
-    img[index].treated = 1;
+void treat_image(ImageToTreat* img, enum EffectType effect){
+    apply_effect(&img->img, &img->out, effect);
+    img->treated = 1;
 }
 
 void display(ImageToTreat* listimg, int count){
@@ -114,15 +110,42 @@ void display(ImageToTreat* listimg, int count){
     }
 }
 
+void* treat_image_thread(void* args){
+    ArgForThread* thread_args = (ArgForThread*) args;
+    int count = thread_args->image_count;
+    ImageToTreat* img = thread_args->images;
+    enum EffectType effect = thread_args->effect;
+    int index;
+    while((index = find_image_to_treat(count, img)) != -1) {
+        printf("index : %d", index);
+        treat_image(&img[index], effect);
+    }
+}
+
+void* write_image_thread(void* args){
+    ArgForThread* thread_args = (ArgForThread*) args;
+    int count = thread_args->image_count;
+    ImageToTreat* img = thread_args->images;
+    enum EffectType effect = thread_args->effect;
+    int index;
+    while((index = find_image_to_write(count, img)) != -1){
+        write_image_to_bitmap(&img[index]);
+    }
+}
+
 int main(int argc, char** argv) {
     ImageToTreat* listimg;
     char* dir = "./images";
     int count = count_img_in_dir(dir);
+    pthread_t threadList[count];
+    pthread_t threadWrite;
     list_image(dir, &listimg, count, "./example");
-
-    for(int i = 0; i < count; i++){
-        treat_image(listimg, count, SHARPEN);
-        //display(listimg, count);
-        write_image_to_bitmap(listimg, count);
-    }
+    ArgForThread* args = malloc(sizeof(int) + (sizeof(ImageToTreat) * count));
+    args->images = listimg;
+    args->image_count = count;
+    args->effect = BOXBLUR;
+    pthread_create(&threadWrite, NULL, treat_image_thread, (void*)args);
+    pthread_join(threadWrite, NULL);
+    pthread_create(&threadList[0], NULL, write_image_thread, (void*)args);
+    pthread_join(threadList[0], NULL);
 }
